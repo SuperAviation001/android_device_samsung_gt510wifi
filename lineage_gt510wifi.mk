43cfb7e177d6848322c36a7c849ad5fdbedd357e
SuperAviation001 <83483383+SuperAviation001@users.noreply.github.com>
2025-03-14 15:03:14 +1100

    gt510wifi: Adapt to LineageOS 22.1
    Build prop overrides have changed in Android 15, so they need to be adapted.

--- lineage_gt510wifi.mk
+++ lineage_gt510wifi.mk
@@ -16,9 +16,8 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-# Set build fingerprint / ID / Product Name ect.
-BUILD_FINGERPRINT=samsung/gt510wifixx/gt510wifi:7.1.1/NMF26X/T550XXU1CQJ6:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
-  TARGET_DEVICE=gt510wifi \
-  PRODUCT_NAME=gt510wifixx \
-  PRIVATE_BUILD_DESC="gt510wifixx-user 7.1.1 NMF26X T550XXU1CQJ6 release-keys"
+  DeviceName=gt510wifi \
+  DeviceProduct=gt510wifixx \
+  BuildDesc="gt510wifixx-user 7.1.1 NMF26X T550XXU1CQJ6 release-keys" \
+  BuildFingerprint=samsung/gt510wifixx/gt510wifi:7.1.1/NMF26X/T550XXU1CQJ6:user/release-keys
